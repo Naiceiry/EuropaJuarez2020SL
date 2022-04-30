0c83370efac910bc51a2f714a8d9a846037627a2
Naiceiry <naiceirymp@gmail.com>
2022-04-30 21:01:07 +0000

    img

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import navbar from "../../img/navbar.jpg";
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbarej">
+      <div className="container">
+        <Link to="/">
+          <img className="imgnavb" src={navbar} />
+        </Link>
+      </div>
+    </nav>
+  );
 };
